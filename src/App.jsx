bac896f5911aa0871503a55905d105dd1b618942
MutumaM <jaydenmwongera@gmail.com>
2026-08-27 16:29:27 +0300

    app.jsx wrapped

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import RestaurantDetail from "./pages/RestaurantDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/restaurant/:placeId" element={<RestaurantDetail />} />
-        <Route path="/neighbourhoods" element={<div>Neighbourhoods page coming soon</div>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/restaurant/:placeId" element={<RestaurantDetail />} />
+          <Route path="/neighbourhoods" element={<div>Neighbourhoods page coming soon</div>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 

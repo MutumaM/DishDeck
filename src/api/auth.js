de0155c07bb00f4738c1314bbac1bb5f6c7f4c7d
MutumaM <jaydenmwongera@gmail.com>
2026-08-28 06:25:10 +0300

    Prep for deployment

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5555";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5555";
 
 export async function registerUser(email, password) {
     const response = await fetch(`${BASE_URL}/api/register`, {

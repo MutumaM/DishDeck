aaa75809bdebb6be89241cdcd88f2afda75bfaa8
MutumaM <jaydenmwongera@gmail.com>
2026-08-28 06:48:46 +0300

    secret_key added and gunicorn installed

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,18 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from models import db, User, Favorite
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dishdeck.db"
+
+db_url = os.environ.get("DATABASE_URL", "sqlite:///dishdeck.db")
+if db_url.startswith("postgres://"):
+      db_url = db_url.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-fallback-key")
 
+
 db.init_app(app)
+with app.app_context():
+    db.create_all()
+
 CORS(app, supports_credentials=True, origins=[
     "http://localhost:5173",
     "https://dish-deck-sigma.vercel.app"
